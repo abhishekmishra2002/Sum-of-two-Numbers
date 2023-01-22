069069c4888df85c59f8b4fd15bd0b1c6ed75118
Abhishek Kumar Mishra <109597832+abhishekmishra2002@users.noreply.github.com>
2023-01-22 15:11:24 +0530

    Add files via upload

---
+++ sum of 2 numbers.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  system("cls");
+  int a,b,sum;
+  cout<<"ENTER THE TWO NUMBERS \n ";
+  cin >>a;
+  cin >>b;
+  sum = a+b;
+  cout<<"THE SUM OF "<<a<<" and "<<b<<" is :"<<sum;
+  return 0;
+}
